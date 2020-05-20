e53059163ecb5914aa44250eabc12f7623e0e2da
KoTeuKou <dima.lapushkin@mail.ru>
2020-05-20 19:29:38 +0400

    fixes

--- src/main/java/com/ssu/carpark/controller/BusCompanyController.java
+++ src/main/java/com/ssu/carpark/controller/BusCompanyController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
-@RequestMapping("/buscompanies")
+@RequestMapping("/bus-companies")
 public class BusCompanyController {
     private final BusCompanyRepository busCompanyRepository;
 
--- src/main/java/com/ssu/carpark/controller/MainController.java
+++ src/main/java/com/ssu/carpark/controller/MainController.java
@@ -2,8 +2,7 @@ package com.ssu.carpark.controller;
 
 import com.ssu.carpark.repository.UserRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,9 +14,11 @@ public class MainController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping
-    public String getAllUsers(Model model) {
-        model.addAttribute("user", userRepository.findAll());
-        return "index";
+    @PostMapping("/login")
+    public String redirect(String param) {
+        if (param.equals("admin")){
+            return "admin";
+        }
+        else return "user";
     }
 }
